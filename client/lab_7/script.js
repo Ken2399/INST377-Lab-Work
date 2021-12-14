4caddfc352bdf99ac2855a3bf186ffad6e612520
Ken2399 <Ken@iskander.myfiosgateway.com>
2021-12-14 00:05:51 -0500

    Fixing mymap

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -5,9 +5,9 @@ async function windowActions() {
 
   const mymap = L.map('mapid').setView([38, -75], 12);
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mymap);
 
   const request = await fetch(endpoint);
